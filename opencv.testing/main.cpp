a63e2ab3d0b75648f1a3d197b8006a9172776d48
tamnq <quangtam.uet@gmail.com>
2021-05-14 21:14:01 +0700

    add sample code opencv

---
+++ opencv.testing/main.cpp
@@ -0,0 +1,11 @@
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/core/core.hpp>
+
+using namespace cv;
+
+int main(int argc, const char* argv[])
+{
+
+}
