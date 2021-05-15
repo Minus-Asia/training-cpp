02af429052ff69b8bfdf281889be6598d0fc2ef8
tamnq <quangtam.uet@gmail.com>
2021-05-15 13:02:32 +0700

    read and show a picture with opencv

--- opencv.testing/main.cpp
+++ opencv.testing/main.cpp
@@ -4,8 +4,27 @@
 #include <opencv2/core/core.hpp>
 
 using namespace cv;
+using namespace std;
 
-int main(int argc, const char* argv[])
+int main(int argc, char** argv)
 {
+    /* Read the image file */
+    Mat image = imread("../index.jpg",
+                       IMREAD_GRAYSCALE);
+  
+    /* Error Handling */
+    if (image.empty()) {
+        cout << "index.jpg "
+             << "Not Found" << endl;
 
+        /* wait for any key press */
+        cin.get();
+        return -1;
+    }
+  
+    // Show Image inside a window
+    imshow("Cat", image);
+  
+    waitKey(0);
+    return 0;
 }
