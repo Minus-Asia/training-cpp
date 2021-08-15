b411f288165b20152afb4c8c166d1410ada6020b
thanh nguyen <nnt296@gmail.com>
2021-08-15 18:24:01 +0700

    multi-thread thanh

--- Multithreading_thanh/main.cpp
+++ Multithreading_thanh/main.cpp
@@ -1,53 +1,68 @@
-#include "bits/stdc++.h"
+#include <iostream>
 #include <thread>
 #include <queue>
 #include <unistd.h>
+#include <mutex>
 
-using namespace std;
-
-#define MAX_VALUE 10
+// Max length of the queues
 #define MAX_QUEUE_SIZE 10
 
-void Producer(queue<int> &inputQueue, int msInterval, int start) {
+[[noreturn]] void Producer(std::queue<int> &inputQueue,
+                           std::mutex &mutex,
+                           int msInterval, int start) {
+    // 2 producer output even and odd values
     int i = start;
-    while (i < MAX_VALUE) {
+    while (true) {
         if (inputQueue.size() < MAX_QUEUE_SIZE) {
+            mutex.lock();
             inputQueue.push(i);
+            mutex.unlock();
             i += 2;
         }
         usleep(msInterval);
     }
 }
 
-void Reader(queue<int> &outputQueue) {
-    while (!outputQueue.empty()) {
-        cout << "Output: " << outputQueue.front() << endl;
-        outputQueue.pop();
+[[noreturn]] void Reader(std::queue<int> &outputQueue) {
+    // Reader consume output thread and print to stdout
+    while (true) {
+        if (!outputQueue.empty()) {
+            std::cout << "Output: " << outputQueue.front() << std::endl;
+            outputQueue.pop();
+        }
     }
 }
 
 // Implementation of Fan-in method from https://divan.dev/posts/go_concurrency_visualize/
-int main() {
-    queue<int> inputQueue;
-    queue<int> outputQueue;
+[[noreturn]] int main() {
+    std::queue<int> inputQueue;
+    std::queue<int> outputQueue;
+    std::mutex mutex;
 
-    // Multiple pop-worker can result in non-thread-safe queue.
-    // Hence, use single output queue only
-    std::thread producer1(Producer, std::ref(inputQueue), 100, 0);
-    std::thread producer2(Producer, std::ref(inputQueue), 250, 1);
+    // Note: BUG small sleep time cause program to freeze NOT SURE WHY
+    // td::ref is required to pass queue to thread
+    std::thread producer1(Producer, std::ref(inputQueue), std::ref(mutex), 100, 0);
+    std::thread producer2(Producer, std::ref(inputQueue), std::ref(mutex), 200, 1);
     std::thread reader(Reader, std::ref(outputQueue));
 
-    while (!inputQueue.empty()) {
-        int value = inputQueue.front();
-        inputQueue.pop();
+    // Main thread will consume producer-threads and output to reader-threads
 
-        outputQueue.push(value);
+    // Multiple pop-worker can result in non-thread-safe queue.
+    while (true) {
+        if (!inputQueue.empty()) {
+            mutex.lock();
+            int value = inputQueue.front();
+            inputQueue.pop();  // This line crash without mutex
+            mutex.unlock();
+
+            if (outputQueue.size() < MAX_QUEUE_SIZE)
+                outputQueue.push(value);
+        }
     }
 
-    // Wait for process to exit
-    producer1.join();
-    producer2.join();
-    reader.join();
-
-    return 0;
+    // // Wait for process to exit
+    // producer1.join();
+    // producer2.join();
+    // reader.join();
+    // return 0;
 }
