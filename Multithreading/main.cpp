90d45130d860b549386d05fec5aaf215558c996b
TamNQ <quangtam.uet@gmail.com>
2021-08-21 15:20:39 +0700

    update multithreading tamnq

--- Multithreading/main.cpp
+++ Multithreading/main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <thread>
-using namespace std;
+#include <semaphore.h>
 
-void Show_Something(string str) {
+using namespace std;
+sem_t pingsem;
+
+void Player(string playerName, int *ball) {
     while(1) {
-        std::cout << str << "\n" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        sem_wait(&pingsem);
+        (*ball)++;
+        std::cout << playerName << ":"<< *ball << "\n" << std::endl;
+        sem_post(&pingsem);
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 }
 
 int main() {
-    std::thread thread_1, thread_2;
+    sem_destroy(&pingsem);
+    sem_init(&pingsem, 0, 1);
 
-    std::cout << "Start MultiThreading\n";
+    std::cout << "Start Ping Pong\n";
+    int ball = 0;
+    std::thread Player1(Player, "Player 1", &ball);
+    std::thread Player2(Player, "Player 2", &ball);
 
-    thread_1 = std::thread(Show_Something, "Thread 1");
-    thread_2 = std::thread(Show_Something, "Thread 2");
-
-    thread_1.join();
-    thread_2.join();
-
+    Player1.join();
+    Player2.join();
     return 0;
 }
