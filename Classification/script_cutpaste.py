237455bd621f523dd0897aa2a35bc9b9030b4630
TamNQ <quangtam.uet@gmail.com>
2022-01-04 22:11:10 +0700

    add code for cut paste polygon

--- Classification/script_cutpaste.py
+++ Classification/script_cutpaste.py
@@ -3,8 +3,6 @@ import os
 import sys
 from random import randint
 import numpy as np
-
-
 import cv2
 
 if __name__ == '__main__':
@@ -38,18 +36,41 @@ if __name__ == '__main__':
         im_defect = cv2.imread(image_defect_path)
         im_good = cv2.imread(image_good_path)
         black_mask = np.zeros_like(im_good)
+        cropped_img = np.zeros_like(im_good)
         for shape in json_obj["shapes"]:
-            label = shape["label"]
-            x1, y1 = list(map(int, shape["points"][0]))
-            x2, y2 = list(map(int, shape["points"][1]))
-            if x2 < x1:
-                x1, x2 = x2, x1
-            if y2 < y1:
-                y1, y2 = y2, y1
+            if shape["shape_type"] == "rectangle":
+                x1, y1 = list(map(int, shape["points"][0]))
+                x2, y2 = list(map(int, shape["points"][1]))
+                if x2 < x1:
+                    x1, x2 = x2, x1
+                if y2 < y1:
+                    y1, y2 = y2, y1
+                im_good[y1: y2, x1: x2] = im_defect[y1: y2, x1: x2]
+                cv2.rectangle(black_mask, (x1, y1), (x2, y2), (255, 255, 255), -1)
+            else:
+                black_mask = cv2.cvtColor(black_mask, cv2.COLOR_BGR2GRAY)
+                # generate mask
+                temp_mask = np.zeros_like(im_good)
+                temp_mask = cv2.cvtColor(temp_mask, cv2.COLOR_BGR2GRAY)
 
-            im_good[y1: y2, x1: x2] = im_defect[y1: y2, x1: x2]
-            cv2.rectangle(black_mask, (x1, y1), (x2, y2), (255, 255, 255), -1)
+                shape_points = np.array(shape["points"]).astype(int)
+                cv2.fillPoly(black_mask, [shape_points], 255)
+                cv2.fillPoly(temp_mask, [shape_points], 255)
+                black_mask = cv2.cvtColor(black_mask, cv2.COLOR_GRAY2BGR)
+                temp_mask = cv2.cvtColor(temp_mask, cv2.COLOR_GRAY2BGR)
+                # crop a peace of image
+                cropped_img = cv2.subtract(temp_mask, im_defect)
+                cropped_img = cv2.subtract(temp_mask, cropped_img)
+                temp_mask_inv = cv2.bitwise_not(temp_mask)
+                im_good = cv2.bitwise_and(temp_mask_inv, im_good)
+                im_good = cv2.bitwise_or(cropped_img, im_good)
 
-        current_num = len(os.listdir(output_dir))
-        cv2.imwrite(os.path.join(output_dir, f"{str(current_num).zfill(4)}.png"), im_good)
-        cv2.imwrite(os.path.join(output_dir, f"{str(current_num).zfill(4)}_mask.png"), black_mask)
+        cv2.imshow("img", black_mask)
+        cv2.imshow("img2", im_good)
+        cv2.waitKey(0)
+        break
+
+        # current_num = len(os.listdir(output_dir))
+        # cv2.imwrite(os.path.join(output_dir, f"{str(current_num).zfill(4)}.png"), im_good)
+        # cv2.imwrite(os.path.join(output_dir, f"{str(current_num).zfill(4)}_mask.png"), black_mask)
+
